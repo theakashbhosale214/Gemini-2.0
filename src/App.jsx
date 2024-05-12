7f45323ec5dd11d063c38b06e2496d1d67c9846a
Akash Bhosale <theakashbhosale214>
2024-05-12 20:09:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Dashbord from "./components/Dashbord/Dashbord";
+
+const App = () => {
+  return (
+    <>
+    <Sidebar></Sidebar>
+    <Dashbord></Dashbord>
+    </>
+  )
+}
+
+export default App
