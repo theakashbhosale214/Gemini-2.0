9ff610f8ff30f92903e18859b52375e10b1de8a9
Akash Bhosale <theakashbhosale214>
2024-05-13 09:21:12 +0530

    front end done

--- src/components/Dashbord/Dashbord.jsx
+++ src/components/Dashbord/Dashbord.jsx
@@ -37,19 +37,24 @@ const Dashbord = () => {
                 </div>
             </div>
 
+
             <div className="main-bottom">
                 <div className="search-box">
                     <input type='text' placeholder='Enter a prompt here' />
-                </div>
+                
+                <div>
                 <img src={assets.gallery_icon} alt="" />
                 <img src={assets.mic_icon} alt="" />
                 <img src={assets.send_icon} alt="" />
+                </div>
+            </div>
+            
+            <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy & Gemini Apps
+            </p>
+
             </div>
         </div>
-
-        <p className="bottom-info">
-        Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy & Gemini Apps
-        </p>
     </div>
   )
 }
