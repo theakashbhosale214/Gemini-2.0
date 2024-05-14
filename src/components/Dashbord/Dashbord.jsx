5604882d1ae4b98d38a64ad22a43974517e6fc0c
Akash Bhosale <theakashbhosale214>
2024-05-14 09:06:54 +0530

    created preloader animation

--- src/components/Dashbord/Dashbord.jsx
+++ src/components/Dashbord/Dashbord.jsx
@@ -48,7 +48,15 @@ const Dashbord = () => {
                     </div>
                     <div className="result-data">
                         <img src={assets.gemini_icon} alt="" />
-                        <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                        {loading                
+                        ? <div className='loader'>
+                            <hr />
+                            <hr />
+                            <hr />
+                        </div>
+                        :
+                        <p dangerouslySetInnerHTML={{__html:resultData}}></p>}
+                        
                     </div>
                   </div>}
             
