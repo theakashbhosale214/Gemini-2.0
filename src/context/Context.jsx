488fae454aba33bd59e25a35f99b0007fa45ee83
Akash Bhosale <theakashbhosale214>
2024-05-14 09:40:04 +0530

    done with typing effect

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,6 +12,11 @@ const ContextProvider = (props) => {
     const[loading,setLoading] = useState(false);
     const[resultData,setResultData] = useState("");
     
+    const delayPara = (index,nextWord) => {
+        setTimeout(function(){
+            setResultData(prev => prev + nextWord);
+        },75*index)
+    }
     
     const onSent = async (prompt) =>{
         setResultData("");
@@ -19,11 +24,26 @@ const ContextProvider = (props) => {
         setShowResult(true);
         setRecentPrompt(input)
         const response = await runChat(input);
-        setResultData(response);
+        let responceArray = response.split("**");
+        let newResponce;
+        for(let i=0;i < responceArray.length;i++){
+            if(i === 0 || i%2 === 0){
+                newResponce += responceArray[i];
+            }else{
+                newResponce += '<b>' + responceArray[i] + '</b>';
+            }
+        }
+        let filterResponce = newResponce.split("*").join("</br>")
+        let newResponceArray = filterResponce.split(" ");
+        for(let i=0;i<newResponceArray.length;i++){
+            const nextWord = newResponceArray[i];
+            delayPara(i,nextWord+" ");
+        }
+        // setResultData(filterResponce);
         setLoading(false);
         setInput("");
     }
-
+    
     const contextValue = {
         prevPrompt,
         setPrevPrompt,
